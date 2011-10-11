57b8467b498858379b88b697a03114f3a7fd1308
wdhae <willem@tcmb.eu>
2011-10-11 09:07:47 +0200

    initial commit

---
+++ TCMB_SuperIntegrated/Form1.vb
@@ -0,0 +1,7 @@
+﻿Public Class Form1
+
+    Private Sub cmdTheButton_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdTheButton.Click
+        MessageBox.Show("This is THE button !", "The button")
+    End Sub
+
+End Class
