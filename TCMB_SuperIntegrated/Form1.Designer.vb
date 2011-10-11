f742838e556c8296d8819643d4e6a88d18147001
wdhae <willem@tcmb.eu>
2011-10-11 15:15:07 +0200

    added listbox

--- TCMB_SuperIntegrated/Form1.Designer.vb
+++ TCMB_SuperIntegrated/Form1.Designer.vb
@@ -29,6 +29,7 @@ Partial Class Form1
         Me.CheckBox1 = New System.Windows.Forms.CheckBox
         Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker
         Me.ProgressBar1 = New System.Windows.Forms.ProgressBar
+        Me.ListBox1 = New System.Windows.Forms.ListBox
         Me.SuspendLayout()
         '
         'cmdTheButton
@@ -75,23 +76,34 @@ Partial Class Form1
         Me.CheckBox1.Text = "TheCheckBox"
         Me.CheckBox1.UseVisualStyleBackColor = True
         '
-        'ProgressBar1
         'DateTimePicker1
+        '
+        Me.DateTimePicker1.Location = New System.Drawing.Point(105, 176)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.Size = New System.Drawing.Size(200, 20)
+        Me.DateTimePicker1.TabIndex = 6
+        '
+        'ProgressBar1
         '
         Me.ProgressBar1.Location = New System.Drawing.Point(266, 93)
         Me.ProgressBar1.Name = "ProgressBar1"
         Me.ProgressBar1.Size = New System.Drawing.Size(100, 23)
         Me.ProgressBar1.TabIndex = 6
-        Me.DateTimePicker1.Location = New System.Drawing.Point(105, 176)
-        Me.DateTimePicker1.Name = "DateTimePicker1"
-        Me.DateTimePicker1.Size = New System.Drawing.Size(200, 20)
-        Me.DateTimePicker1.TabIndex = 6
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(105, 227)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(120, 95)
+        Me.ListBox1.TabIndex = 7
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(409, 363)
+        Me.Controls.Add(Me.ListBox1)
         Me.Controls.Add(Me.ProgressBar1)
         Me.Controls.Add(Me.DateTimePicker1)
         Me.Controls.Add(Me.CheckBox1)
@@ -112,5 +124,6 @@ Partial Class Form1
     Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
     Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
     Friend WithEvents DateTimePicker1 As System.Windows.Forms.DateTimePicker
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
 
 End Class
