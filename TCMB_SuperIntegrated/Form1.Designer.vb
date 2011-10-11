47ab14a724c595db3389c496b54dc7445e6c5b84
wdhae <willem@tcmb.eu>
2011-10-11 09:12:21 +0200

    Added THE textbox!

--- TCMB_SuperIntegrated/Form1.Designer.vb
+++ TCMB_SuperIntegrated/Form1.Designer.vb
@@ -23,6 +23,8 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.cmdTheButton = New System.Windows.Forms.Button
+        Me.TheTextBox = New System.Windows.Forms.TextBox
+        Me.lblTheTextBox = New System.Windows.Forms.Label
         Me.SuspendLayout()
         '
         'cmdTheButton
@@ -34,17 +36,38 @@ Partial Class Form1
         Me.cmdTheButton.Text = "The Button"
         Me.cmdTheButton.UseVisualStyleBackColor = True
         '
+        'TheTextBox
+        '
+        Me.TheTextBox.Location = New System.Drawing.Point(105, 96)
+        Me.TheTextBox.Name = "TheTextBox"
+        Me.TheTextBox.Size = New System.Drawing.Size(133, 20)
+        Me.TheTextBox.TabIndex = 1
+        '
+        'lblTheTextBox
+        '
+        Me.lblTheTextBox.AutoSize = True
+        Me.lblTheTextBox.Location = New System.Drawing.Point(102, 76)
+        Me.lblTheTextBox.Name = "lblTheTextBox"
+        Me.lblTheTextBox.Size = New System.Drawing.Size(101, 13)
+        Me.lblTheTextBox.TabIndex = 2
+        Me.lblTheTextBox.Text = "Put your texts here !"
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(312, 271)
+        Me.Controls.Add(Me.lblTheTextBox)
+        Me.Controls.Add(Me.TheTextBox)
         Me.Controls.Add(Me.cmdTheButton)
         Me.Name = "Form1"
         Me.Text = "TCMB_SuperIntegrated"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
     Friend WithEvents cmdTheButton As System.Windows.Forms.Button
+    Friend WithEvents TheTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents lblTheTextBox As System.Windows.Forms.Label
 
 End Class
